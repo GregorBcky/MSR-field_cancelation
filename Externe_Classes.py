dabcd3dee4265b25dc8142db5fd8bc2f6e4a06eb
Gregor Bock <gregor.bock02@gmail.com>
2026-04-30 18:42:25 +0200

    B-field_prediction_without_mu as method "coil_field_by_Biot_Savart" of class Coil_Layup

--- Externe_Classes.py
+++ Externe_Classes.py
@@ -1,17 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
+
 import matplotlib.tri as mtri
 import trimesh
 import bfieldtools
 from bfieldtools.utils import combine_meshes
 
+import scipy.constants
+mu_0=scipy.constants.mu_0
+
 class Coil_Layup():
-    def __init__(self, coil_diameter, x_dist, y_dist, z_dist, n_windings, coil_plane_dist_to_origin_x, coil_plane_dist_to_origin_y, coil_plane_dist_to_origin_z, usable_length_x, usable_length_y, usable_length_z):
+    def __init__(self, coil_diameter, x_dist, y_dist, z_dist, n_windings, current, coil_plane_dist_to_origin_x, coil_plane_dist_to_origin_y, coil_plane_dist_to_origin_z, usable_length_x, usable_length_y, usable_length_z):
         self.coil_diameter = coil_diameter
         self.x_dist = x_dist
         self.y_dist = y_dist
         self.z_dist = z_dist
         self.n_windings = n_windings
+        self.current = current
         self.coil_plane_dist_to_origin_x = coil_plane_dist_to_origin_x
         self.coil_plane_dist_to_origin_y = coil_plane_dist_to_origin_y
         self.coil_plane_dist_to_origin_z = coil_plane_dist_to_origin_z
@@ -141,15 +145,65 @@ class Coil_Layup():
         # Handle n_windings: scalar → broadcast OR array
         if np.isscalar(n_windings) or (hasattr(n_windings, 'shape') and n_windings.shape == ()):
             # Scalar: replicate für alle Coils
-            self.n_windings_per_coil = np.full(self.all_coils.shape[0], n_windings, dtype=int)
+            self.n_windings = np.full(self.all_coils.shape[0], n_windings, dtype=int)
         elif isinstance(n_windings, (np.ndarray, list)):
             # Array: check length
             if len(n_windings) == self.all_coils.shape[0]:
-                self.n_windings_per_coil = np.array(n_windings, dtype=int)
+                self.n_windings = np.array(n_windings, dtype=int)
             else:
                 raise ValueError(f"n_windings length {len(n_windings)} != n_coils {self.all_coils.shape[0]}")
         else:
             raise ValueError("n_windings must be scalar, list or np.array")
+        
+        if np.isscalar(current) or (hasattr(current, 'shape') and current.shape == ()):
+            # Scalar: replicate für alle Coils
+            self.current = np.full(self.all_coils.shape[0], current, dtype=float)
+        elif isinstance(current, (np.ndarray, list)):
+            # Array: check length
+            if len(current) == self.all_coils.shape[0]:
+                self.current = np.array(current, dtype=float)
+            else:
+                raise ValueError(f"current length {len(current)} != n_coils {self.all_coils.shape[0]}")
+        else:
+            raise ValueError("current must be scalar, list or np.array")
+        
+        self.I_windings_total = self.current * self.n_windings  # (n_coils_total,)
+
+
+    def coil_field_by_Biot_Savart(self, target_points):
+            """
+            target_points: (n_targets, 3)
+            coils: (n_coils, n_points_per_coil, 3)
+                Each coil must be closed or will be closed by connecting last to first.
+            I_windings_total: (n_coils,) array of total currents (current * windings) for each coil
+            returns: (n_targets, 3)
+            """
+
+            # Segment vectors along each coil
+            dR = np.roll(self.all_coils, -1, axis=1) - self.all_coils                      # (n_coils, n_seg, 3); difference between consecutive points along each coil, with wrap-around for closed loop
+            ds = np.linalg.norm(dR, axis=2, keepdims=True)               # (n_coils, n_seg, 1); length of each circle segment computed from geometry
+
+            # Current element dℓ = ds * tangent_hat, here tangent_hat = dR / |dR|
+            # so dℓ = dR. Multiply by current and winding count.
+            dl = self.I_windings_total[:, None, None] * dR                    # (n_coils, n_seg, 3); current * windings * tangential sector vector (not normalized, since ds is included)
+
+            B = np.zeros((target_points.shape[0], 3), dtype=float)
+
+            for i, target in enumerate(target_points):
+                r = target[None, None, :] - self.all_coils                        # (n_coils, n_seg, 3); $\vec{r}-\vec{r}'$
+                r_norm = np.linalg.norm(r, axis=2)                       # (n_coils, n_seg);    $|\vec{r}-\vec{r}'|$
+
+                # Avoid division by zero if a target lies exactly on a segment point
+                if np.any(r_norm <= 0):
+                    print(f'WARNING: r_norm = {r_norm} which leads to a division by zero in the Biot-Savart calculation (Point {i})')
+
+                cross = np.cross(dl, r)                                  # (n_coils, n_seg, 3); $\mathrm{d}\vec{\ell} \cross (\vec{r}-\vec{r}')$
+                contrib = np.zeros_like(cross)
+                contrib = cross / (r_norm**3)[:, :, None]                # Note that r_norm has shape (n_coils, n_seg) and cross has shape (n_coils, n_seg, 3), therefore the axis for broadcasting are needed)
+
+                B[i] = mu_0 / (4 * np.pi) * np.sum(contrib, axis=(0, 1))
+
+            return B
 
 class Mu_material():
     def __init__(self, shield_dim, shield_thickness):
@@ -204,7 +258,7 @@ class Mu_material():
         z_shield = tri_xz_shield.vertices[:, 2]
         faces_shield = tri_xz_shield.faces
         # Create triangulation object
-        triang_shield = mtri.Triangulation(x_shield, z_shield, faces_shield)
+        self.triang_shield = mtri.Triangulation(x_shield, z_shield, faces_shield)
 
         shield_loc = self.shield_dim / 2                                          # Location of the shield from the center (normal to surface) = 1/2 shield size (MSR is closed) -> compare to coil distance
         plus_off_xy_shield=np.array([0,0,shield_loc])
@@ -233,41 +287,16 @@ class Mu_material():
             tri_yz_shield.vertices+minus_off_yz_shield,tri_yz_shield.faces,process=False
         )
 
-        total_shield = combine_meshes((
+        self.total_shield = combine_meshes((
             shield_plus_xy, shield_minus_xy,
             shield_plus_xz, shield_minus_xz, 
             shield_plus_yz, shield_minus_yz
         ))
 
         # CRUCIAL: Repair seams/duplicates
-        total_shield.merge_vertices()  # Merge nahe Vertices (Kanten)
-        total_shield.fix_normals()
+        self.total_shield.merge_vertices()  # Merge nahe Vertices (Kanten)
+        self.total_shield.fix_normals()
         # total_shield.fill_holes()  # Falls Löcher
 
         # Jetzt: Keine Boundary-Edges mehr (closed manifold)
-        inner_idx_shield = bfieldtools.utils.find_mesh_boundaries(total_shield)
-        print("Inner vertices nach Repair:", len(inner_idx_shield[0]))  # Sollte fast alle sein
-
-
-        # From here on, plotting starts!!!
-        fig, ax = plt.subplots(figsize=(6,6))
-        ax.triplot(triang_shield, color='k', linewidth=0.8)
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        inner_idx_shield=bfieldtools.utils.find_mesh_boundaries(total_shield)
-        inner_vertex_idx_shield=inner_idx_shield[0]
-        for idx in inner_vertex_idx_shield:
-            boundary=total_shield.vertices[idx]
-            ax.scatter(boundary[:,0],boundary[:,1],boundary[:,2])
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        verts_shield=total_shield.vertices
-        ax.scatter(verts_shield[:,0],verts_shield[:,1],verts_shield[:,2],alpha=0.5)
-
-        points_inside = total_shield.vertices - self.shield_thickness * total_shield.vertex_normals
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(verts_shield[:,0],verts_shield[:,1],verts_shield[:,2],alpha=0.3)
-        ax.scatter(points_inside[:,0],points_inside[:,1],points_inside[:,2],alpha=0.3)
+        inner_idx_shield = bfieldtools.utils.find_mesh_boundaries(self.total_shield)
